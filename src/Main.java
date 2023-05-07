05f0c9b6e21a1b89fc105e881591373d310918aa
Aleksey909 <asilkin250@gmail.com>
2023-05-08 02:17:48 +0300

    OOP2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("Car1", 4);
+        Car car2 = new Car("Car2", 4);
+
+        Truck truck1 = new Truck("truck1", 6);
+        Truck truck2 = new Truck("truck2", 8);
+
+        Bicycle bicycle1 = new Bicycle("bicycle2", 2);
+        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+
+        Transport[] transports = {
+                car1,
+                car2,
+                truck1,
+                truck2,
+                bicycle1,
+                bicycle2
+        };
+
+        for (Transport t: transports){
+            t.servers();
+            System.out.println( );
+        }
     }
 }
